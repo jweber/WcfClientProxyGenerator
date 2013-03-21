906617ff47dbdd6dbd15228c8f314c2bd91827e3
jweber <john@jweber.org>
2013-03-20 20:57:08 -0700

    Moved caching of ChannelFactories into the ChannelFactoryProvider class

---
+++ source/WcfClientProxyGenerator.Tests/ChannelFactoryProviderTests.cs
@@ -0,0 +1,56 @@
+﻿using System.ServiceModel;
+using NUnit.Framework;
+using WcfClientProxyGenerator.Tests.Infrastructure;
+
+namespace WcfClientProxyGenerator.Tests
+{
+    [TestFixture]
+    public class ChannelFactoryProviderTests
+    {
+        [Test]
+        public void ChannelFactories_WithIdenticalConfiguration_AreSameInstance_ForCodeBasedConfiguration()
+        {
+            var factory1 = ChannelFactoryProvider.GetChannelFactory<ITestService>(
+                new WSHttpBinding(), 
+                new EndpointAddress("http://localhost:23456/TestService"));
+
+            var factory2 = ChannelFactoryProvider.GetChannelFactory<ITestService>(
+                new WSHttpBinding(), 
+                new EndpointAddress("http://localhost:23456/TestService"));
+
+            Assert.AreSame(factory1, factory2);
+        }
+        
+        [Test]
+        public void ChannelFactories_WithNonIdenticalConfiguration_AreNotSameInstance_ForCodeBasedConfiguration()
+        {
+            var factory1 = ChannelFactoryProvider.GetChannelFactory<ITestService>(
+                new WSHttpBinding(), 
+                new EndpointAddress("http://localhost:23456/TestService"));
+
+            var factory2 = ChannelFactoryProvider.GetChannelFactory<ITestService>(
+                new WSHttpBinding(), 
+                new EndpointAddress("http://localhost:23456/TestService2"));
+
+            Assert.AreNotSame(factory1, factory2);
+        }
+
+        [Test]
+        public void ChannelFactories_WithIdenticalConfiguration_AreSameInstance_ForEndpointConfigurationName()
+        {
+            var factory1 = ChannelFactoryProvider.GetChannelFactory<ITestService>("ITestService");
+            var factory2 = ChannelFactoryProvider.GetChannelFactory<ITestService>("ITestService");
+
+            Assert.AreSame(factory1, factory2);
+        }
+
+        [Test]
+        public void ChannelFactories_WithNonIdenticalConfiguration_AreNotSameInstance__ForEndpointConfigurationName()
+        {
+            var factory1 = ChannelFactoryProvider.GetChannelFactory<ITestService>("ITestService");
+            var factory2 = ChannelFactoryProvider.GetChannelFactory<ITestService>("ITestService2");
+
+            Assert.AreNotSame(factory1, factory2);
+        }
+    }
+}
---
+++ source/WcfClientProxyGenerator/ChannelFactoryProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace WcfClientProxyGenerator
+{
+    internal static class ChannelFactoryProvider
+    {
+        private static readonly ConcurrentDictionary<string, Lazy<object>> _channelFactoryCache;
+
+        static ChannelFactoryProvider()
+        {
+            _channelFactoryCache = new ConcurrentDictionary<string, Lazy<object>>();
+        }
+
+        public static ChannelFactory<TServiceInterface> GetChannelFactory<TServiceInterface>(string endpointConfigurationName)
+            where TServiceInterface : class
+        {
+            string cacheKey = GetCacheKey<TServiceInterface>(endpointConfigurationName);
+            var channelFactory = GetOrAddToCache(
+                cacheKey,
+                () => new ChannelFactory<TServiceInterface>(endpointConfigurationName));
+
+            return channelFactory;
+        }
+
+        public static ChannelFactory<TServiceInterface> GetChannelFactory<TServiceInterface>(Binding binding, EndpointAddress endpointAddress)
+            where TServiceInterface : class
+        {
+            string cacheKey = GetCacheKey<TServiceInterface>(binding, endpointAddress);
+            var channelFactory = GetOrAddToCache(
+                cacheKey,
+                () => new ChannelFactory<TServiceInterface>(binding, endpointAddress));
+
+            return channelFactory;
+        }
+
+        private static ChannelFactory<TServiceInterface> GetOrAddToCache<TServiceInterface>(
+            string cacheKey,
+            Func<ChannelFactory<TServiceInterface>> factory)
+        {
+            Lazy<object> lazy = _channelFactoryCache.GetOrAdd(cacheKey, new Lazy<object>(factory));
+            return lazy.Value as ChannelFactory<TServiceInterface>;
+        }
+
+        private static string GetCacheKey<TServiceInterface>(string endpointConfigurationName)
+        {
+            return string.Format("type:{0};config:{1}",
+                                 typeof(TServiceInterface).FullName,
+                                 endpointConfigurationName);
+        }
+
+        private static string GetCacheKey<TServiceInterface>(Binding binding, EndpointAddress endpointAddress)
+        {
+            return string.Format("type:{0};binding:{1};uri:{2}",
+                                 typeof(TServiceInterface).FullName,
+                                 binding.Name,
+                                 endpointAddress);
+        }
+    }
+}
